847b6b7cb81648b4cc07ec801672f064c0295d56
Pavel V. Pristupa <pristupa@gmail.com>
2019-01-24 20:06:38 +0700

    Change version to 0.1.0, update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
+from setuptools import find_packages
 from setuptools import setup
 
 setup(
     name='ddd',
-    version='0.1',
-    description='Domain-Driven Design for Python',
-    url='https://github.com/pristupa/ddd',
+    version='0.1.0',
     author='Pavel V. Pristupa',
     author_email='pristupa@gmail.com',
+    packages=find_packages(exclude=['tests*']),
+    scripts=[],
+    url='https://github.com/pristupa/ddd',
     license='MIT',
-    packages=['ddd'],
+    description='Domain-Driven Design for Python',
+    long_description=open('README.txt').read(),
     install_requires=[],
+    python_requires=">=3.6",
 )
