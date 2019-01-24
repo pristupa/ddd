b32263219f9746cd625b0f248416c8b00a3fee74
Pavel V. Pristupa <pristupa@gmail.com>
2019-01-24 19:40:38 +0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ddd',
+    version='0.1',
+    description='Domain-Driven Design for Python',
+    url='https://github.com/pristupa/ddd',
+    author='Pavel V. Pristupa',
+    author_email='pristupa@gmail.com',
+    license='MIT',
+    packages=['ddd'],
+    install_requires=[],
+)
