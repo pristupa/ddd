e8cf8d7d2b47085f223983c192243439c6ee7a84
Pavel V. Pristupa <pristupa@gmail.com>
2019-01-25 19:36:30 +0700

    Fix bug with process_domain_events, add test coverage

--- ddd/__init__.py
+++ ddd/__init__.py
@@ -4,5 +4,3 @@ from .domain_event_handler import domain_event_handler
 from .entity import Entity
 from .process_domain_events import process_domain_events
 from .value_object import ValueObject
-
-__version__ = '0.1.1'
--- ddd/process_domain_events.py
+++ ddd/process_domain_events.py
@@ -11,18 +11,24 @@ class MaximumRecursionException(Exception):
     pass
 
 
-def process_domain_events(entities: Iterable[Entity], domain_events: DomainEventCollection = None):
+def process_domain_events(entities: Iterable[Entity], additional_domain_events: DomainEventCollection = None):
     depth = 0
     while True:
+        domain_events = []
+
         for entity in entities:
-            for domain_event in entity.domain_events:
-                handle_domain_event(domain_event)
+            domain_events.extend(entity.domain_events)
             entity.domain_events.clear()
 
-        if domain_events:
-            for domain_event in domain_events:
-                handle_domain_event(domain_event)
-            domain_events.clear()
+        if additional_domain_events:
+            domain_events.extend(additional_domain_events)
+            additional_domain_events.clear()
+
+        if not domain_events:
+            break
+
+        for domain_event in domain_events:
+            handle_domain_event(domain_event)
 
         depth += 1
         if depth > MAXIMUM_RECURSION_DEPTH:
