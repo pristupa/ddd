01dff47e4a2dbb10639ca0ee7bc368dc6b167317
Alexander Egorov <mofr@zond.org>
2019-04-15 17:24:30 +0700

    Create AggregateRoot._domain_events lazily

--- ddd/aggregate_root.py
+++ ddd/aggregate_root.py
@@ -2,9 +2,15 @@ from .domain_events import DomainEvents
 
 
 class AggregateRoot:
-    def __init__(self):
-        super().__init__()
-        self.domain_events = DomainEvents()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._domain_events: DomainEvents = None
+
+    @property
+    def domain_events(self) -> DomainEvents:
+        if getattr(self, '_domain_events', None) is None:
+            self._domain_events = DomainEvents()
+        return self._domain_events
 
     def clear_domain_events(self):
-        self.domain_events.clear()
+        self._domain_events.clear()
